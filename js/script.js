0df3758a6c6d27db919fd75234484f48f2da87aa
Myroslav <myroslavtp@gmail.com>
2022-10-09 09:09:51 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+
+    /// BUTTON
+    
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+    
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
+
+
+    /// SLIDER main 
+
+$('.slider').slick({
+    arrows:false,
+    dots:true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 1,
+    autoplaySpeed: 4000,
+    adaptiveHeight: true,
+    autoplay: false,
+    fade: true,
+    cssEase: 'linear'
+  })
+
+
+  /// SLIDER Guitars
+
+  $('.slider-news').slick({
+    dots: false,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 4,
+    adaptiveHeight: true,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    draggable: true,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='./img/prev.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='./img/next.png'>",
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+        slidesToShow: 3,
+        centerMode: false, 
+        dots: false,
+        arrows: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+        slidesToShow: 1,
+        centerMode: false, 
+        dots: true,
+        arrows: false,
+        }
+      }
+     ]
+  });
+
+
+  ////Gallery
+
+  $('.gallery-container').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+        verticalFit: true
+    },
+    zoom: {
+        enabled: true,
+        duration: 300 // don't foget to change the duration also in CSS
+    }
+});
+
+
+/////Map 
+
+
+function initMap() {
+
+  const CENTER = { lat: 50.433535625228124, lng: 30.522252965466034 };
+  const MAP = new google.maps.Map(document.getElementById("map"), {
+      center: CENTER,
+      zoom: 13
+    });
+
+  }
+
+
+
+
+
+
+
+
+});
